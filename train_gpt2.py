678b7a09a144cc687fd547520f40beb005a2191b
wqin <127051611+wqin2@users.noreply.github.com>
2024-11-16 19:10:30 -0500

    added weight sharing scheme

--- train_gpt2.py
+++ train_gpt2.py
@@ -92,6 +92,9 @@ class GPT(nn.Module):
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False) # gpt uses no bias for the final projection
 
+        # weight sharing scheme
+        self.transformer.wte.weight = self.lm_head.weight
+
     def forward(self, idx, targets=None):
         # idx is of shape (B, T); input of the model is token indices
         B, T = idx.size()
