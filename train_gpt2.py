fe3d8681c97c4c540ac838b4943b6077da780097
wqin <127051611+wqin2@users.noreply.github.com>
2024-11-16 23:32:59 -0500

    torch compile and using bf16 for training

--- train_gpt2.py
+++ train_gpt2.py
@@ -212,7 +212,10 @@ class DataLoaderLite:
             self.current_position = 0
         return x, y
 
+# ---------------------------------------------------------------------
 # autodetect the device
+import time
+
 device = 'cpu'
 if torch.cuda.is_available():
     device = 'cuda'
@@ -223,8 +226,10 @@ print(f"using device: {device}")
 torch.manual_seed(1337)
 if torch.cuda.is_available():
     torch.cuda.manual_seed(1337)
-    
-train_loader = DataLoaderLite(B=4, T=32)
+
+train_loader = DataLoaderLite(B=16, T=1024)
+
+torch.set_float32_matmul_precision('high')
 
 # get logits
 # load the pretrained gpt2 model
@@ -232,17 +237,24 @@ train_loader = DataLoaderLite(B=4, T=32)
 # load a randomly initialized model
 model = GPT(GPTConfig())
 model.to(device)
+model = torch.compile(model)
 
 # optimize
 optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
 for i in range(50):
+    t0 = time.time()
     x, y = train_loader.next_batch()
     x, y = x.to(device), y.to(device)
     optimizer.zero_grad()
-    logits, loss = model(x, y)
+    with torch.autocast(device_type=device, dtype=torch.bfloat16):
+        logits, loss = model(x, y)
     loss.backward()
     optimizer.step()
-    print(f"step {i}, loss: {loss.item()}")
+    torch.cuda.synchronize() # wait for the GPU to finish work
+    t1 = time.time()
+    dt = (t1 - t0) * 1000 # miliseconds
+    tokens_per_sec = (train_loader.B * train_loader.T) / (t1 - t0)
+    print(f"step {i}, loss: {loss.item()}, dt: {dt:.2f}ms, tok/sec: {tokens_per_sec:.2f}")
 
 import sys; sys.exit(0)
 
